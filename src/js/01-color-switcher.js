40604769d535a3517da02ac952760f8dea06e607
Yurkes2020 <yurkes2020@gmail.com>
2022-08-29 18:57:45 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,8 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const start = document.querySelector('[data-start]');
+console.log(start);
+const stop = document.querySelector('[data-stop]');
+console.log(stop);
