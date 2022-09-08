d846dc7ca399db230b08e7d52e61d3f3d57f27b0
Yurkes2020 <yurkes2020@gmail.com>
2022-09-08 22:36:04 +0300

    last

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,27 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise(function (resolve, reject) {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
 }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.success(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
+btnRef = document.querySelector('button');
+inpDelay = document.querySelector('input[name="delay"]');
+inpStep = document.querySelector('input[name="step"]');
+inpAmount = document.querySelector('input[name="amount"]');
+
+btnRef.addEventListener('click', createPromise);
