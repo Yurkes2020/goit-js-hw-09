1942ce49ef8c7455839c38595f0b6aba17a53469
Yurkes2020 <104568412+Yurkes2020@users.noreply.github.com>
2022-09-10 15:07:15 +0000

    Deploying to gh-pages from @ Yurkes2020/goit-js-hw-09@fcccb7e180d2f68cd2d0b53bbd200efff6a33566 🚀

---
+++ 01-color-switcher.8515cc04.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){n=setInterval((function(){return document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,e.disabled=!0})),e.disabled=!0;var n=null}();
+//# sourceMappingURL=01-color-switcher.8515cc04.js.map
